67deb5213f7bf4b6dd206e79487e2974bd2e74fa
steven66619 <70050577+steven66619@users.noreply.github.com>
2020-09-10 05:54:16 +0100

    Add files via upload
    personal repo but hopefully it helps others

---
+++ subvol.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# setting up btrfs
+cfdisk /dev/sda
+mkfs.btrfs -L root /dev/sda2
+mkfs.fat -F32 /dev/sda1
+#mounting the drive to setup subvolumes
+mount /dev/sda2
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@btrfs
+btrfs su cr /mnt/@snapshots
+# mount drive
+umount /dev/sda2
+# mounting subvvolumes
+mount -o compress=lzo,relatime,subvol=@ /dev/sda2 /mnt
+mount -o compress=lzo,relatime,subvol=@home /dev/sda2 /mnt/@home
+mount -o compress=lzo,relatime,subvolid=5 /dev/sda2 /mnt/@btrfs
+# mount boot partition 
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+# thank you for using this script 
+echo $'\n'$"*** All done with subvolumes. ***"
