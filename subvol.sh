79b00dc23f46175fe4bd683d09517692591720ba
steven atkinson <steven4x4@gmail.com>
2022-12-06 08:06:41 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   subvol.sh

--- subvol.sh
+++ subvol.sh
@@ -6,7 +6,7 @@ cfdisk /dev/sda
 mkfs.btrfs -L root /dev/sda2
 mkfs.fat -F32 /dev/sda1
 #mounting the drive to setup subvolumes
-mount /dev/sda2
+mount /dev/sda2 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
 btrfs su cr /mnt/@btrfs
@@ -15,6 +15,7 @@ btrfs su cr /mnt/@snapshots
 umount /dev/sda2
 # mounting subvvolumes
 mount -o compress=lzo,relatime,subvol=@ /dev/sda2 /mnt
+mkdir /mnt/home
 mount -o compress=lzo,relatime,subvol=@home /dev/sda2 /mnt/@home
 mount -o compress=lzo,relatime,subvolid=5 /dev/sda2 /mnt/@btrfs
 # mount boot partition 
